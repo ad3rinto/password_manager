46b249709d03d3695a2efa75c2779b8a8ce1dabc
ad3rinto <aad3rinto@gmail.com>
2023-11-29 09:25:35 +0000

    NFI

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager Agba")
+window.config(pady=20, padx=20)
+key_img = PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200, highlightthickness=0, )
+canvas.create_image(100, 100, image=key_img)
+canvas.pack()
+
+window.mainloop()
